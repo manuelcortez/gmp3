48630752f12d273b58520b4d0639cf57db14b757
Chris Norman <chris.norman2@googlemail.com>
2016-08-08 00:42:21 +0100

    Began to add logging.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 """The main entry."""
 
 if __name__ == '__main__':
- import db
- db.Base.metadata.create_all()
- from gui.main_frame import MainFrame
- import application
- application.frame = MainFrame(None)
- application.frame.Show(True)
- application.frame.Maximize()
- application.app.MainLoop()
+ from default_argparse import parser
+ args = parser.parse_args()
+ import logging
+ logging.basicConfig(stream = args.log_file, level = args.log_level)
+ try:
+  import application
+  logging.info('Starting %s, version %s.', application.name, application.__version__)
+  import db
+  db.Base.metadata.create_all()
+  from gui.main_frame import MainFrame
+  application.frame = MainFrame(None)
+  application.frame.Show(True)
+  application.frame.Maximize()
+  application.app.MainLoop()
+  logging.info('Done.')
+ except Exception as e:
+  logging.exception(e)
