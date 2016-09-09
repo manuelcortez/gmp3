b645c8145a1e0f7138c07852cd89666bda70b976
Chris Norman <chris.norman2@googlemail.com>
2016-09-09 12:29:08 +0100

    Added the ability to play the song with the provided ID.

--- gui/menus/source.py
+++ gui/menus/source.py
@@ -1,15 +1,18 @@
 """Source menu."""
 
-import wx, showing
+import wx, showing, application
 from threading import Thread
+from gmusicapi.exceptions import CallFailure, NotLoggedIn
 from db import session, Track
 from functions.google import playlist_action
+from functions.util import do_login, do_error
 from .base import BaseMenu
 
 class SourceMenu(BaseMenu):
  """The source menu."""
  def __init__(self, frame):
   self.name = '&Source'
+  self.frame = frame
   super(SourceMenu, self).__init__()
   frame.Bind(wx.EVT_MENU, lambda event: Thread(target = frame.load_library,).start(), self.Append(wx.ID_ANY, '&Library\tCTRL+L', 'Load every song in your Google Music library.'))
   frame.Bind(wx.EVT_MENU, lambda event: Thread(target = frame.load_promoted_songs).start(), self.Append(wx.ID_ANY, 'Promoted &Songs\tCTRL+3', 'Load promoted songs.'))
@@ -26,3 +29,22 @@ class SourceMenu(BaseMenu):
   frame.stations_menu.AppendSubMenu(frame.delete_stations_menu, '&Delete')
   self.AppendSubMenu(frame.stations_menu, '&Radio Stations', 'Locally stored and remote radio stations.')
   frame.Bind(wx.EVT_MENU, lambda event: setattr(frame, 'autoload', [frame.autoload[0]] if frame.autoload else []), self.Append(wx.ID_ANY, 'St&op Loading Results', 'Stop loading results to the track view.'))
+  frame.Bind(wx.EVT_MENU, self.load_track, self.Append(wx.ID_ANY, 'Load Specific Track...\tCTRL+SHIFT+I', 'Load a track with a specific ID.'))
+ 
+ def load_track(self, event, id = None):
+  """Load a track by ID."""
+  if id is None:
+   dlg = wx.TextEntryDialog(self.frame, 'Enter the ID of the track to view', 'Track ID')
+   if dlg.ShowModal() == wx.ID_OK:
+    id = dlg.GetValue()
+   else:
+    id = None
+   dlg.Destroy()
+  if id is not None:
+   try:
+    info = application.api.get_track_info(id)
+   except CallFailure:
+    return do_error('Invalid track ID: %s.' % id)
+   except NotLoggedIn:
+    return do_login(callback = self.load_track, args = [event], kwargs = dict(id = id))
+   self.frame.add_results([info], showing = 'ID: %s' % id)
