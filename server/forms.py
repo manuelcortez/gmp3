297bcd88236b10ca5b5b7fae2993f88bced8325c
Chris Norman <chris.norman2@googlemail.com>
2016-10-19 22:49:27 +0100

    Removed the accidentally-left-around js-cookie.

--- server/forms.py
+++ server/forms.py
@@ -1,10 +1,6 @@
 """Forms for the web-facing portion of GMP3."""
 
-from wtforms import Form, StringField, PasswordField, validators
+from wtforms import Form, StringField, validators
 
 class SearchForm(Form):
  search = StringField('Search', [validators.length(min = 1)])
-
-class LoginForm(Form):
- username = StringField('username')
- password = PasswordField('password')
