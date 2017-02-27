8e8bb80d4584fe8eac8aa27070d4f69d874519a6
Chris Norman <chris.norman2@googlemail.com>
2017-02-27 22:13:06 +0000

    Pep8.

--- make-installer.py
+++ make-installer.py
@@ -3,15 +3,15 @@
 import application
 from jinja2 import Environment, FileSystemLoader
 
-environment = Environment(loader = FileSystemLoader('.'))
+environment = Environment(loader=FileSystemLoader('.'))
 
 if __name__ == '__main__':
- with open('installer.iss', 'w') as f:
-  f.write(
-   environment.get_template('installer.iss.template').render(
-    app_name = application.name,
-    app_version = application.__version__
-   )
-  )
- import os
- os.system('start installer.iss &')
+    with open('installer.iss', 'w') as f:
+        f.write(
+            environment.get_template('installer.iss.template').render(
+                app_name=application.name,
+                app_version=application.__version__
+            )
+        )
+    import os
+    os.system('start installer.iss &')
