08e728f07a67e6eb41f807a6451b92a581be11ce
Chris Norman <chris.norman2@googlemail.com>
2017-02-27 22:11:35 +0000

    Fixed pyglet validator for Pep8 compliance.

--- pyglet_validator.py
+++ pyglet_validator.py
@@ -2,12 +2,13 @@
 
 from simpleconf.validators import ValidationError, Boolean
 
+
 class PygletValidator(Boolean):
- def validate(self, option):
-  super(PygletValidator, self).validate(option)
-  if option.value:
-   try:
-    import pyglet
-    assert pyglet
-   except (ImportError, AssertionError):
-    raise ValidationError('Pygame is not installed.')
+    def validate(self, option):
+        super(PygletValidator, self).validate(option)
+        if option.value:
+            try:
+                import pyglet
+                assert pyglet  # Silence flake8.
+            except (ImportError, AssertionError):
+                raise ValidationError('Pygame is not installed.')
